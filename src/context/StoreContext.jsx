1602314232b654eb9c588a69fa47004f8085e057
RS-233 <146667095+RS-233@users.noreply.github.com>
2024-06-02 21:09:37 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url =`http://yumrun-1.onrender.com`
+    const url =`https://yumrun-1.onrender.com`
     const [token,setToken] = useState("")
     const [food_list,setFoodList] = useState([])
 
