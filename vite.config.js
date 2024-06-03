458e03d7a6a339d28a7f196c565f3e9b2b00a3fd
RS-233 <146667095+RS-233@users.noreply.github.com>
2024-06-03 09:24:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      "/user":"https://yumrun-1.onrender.com"
-    }
-  }
   plugins: [react()],
 })
